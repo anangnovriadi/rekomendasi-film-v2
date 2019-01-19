641f5aeb4f944dbb4ffe1113ebcfa974c569fd5c
anangnov99@gmail.com <anangnov99@gmail.com>
2019-01-19 16:00:11 +0700

    header admin

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -38,7 +38,7 @@
             <ul class="navbar-nav my-lg-0">
                 <!-- ============================================================== -->
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="{{ asset('admin/images/users/1.jpg') }}" alt="user" class="profile-pic" /></a>
+                    <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="{{ asset('admin/images/user.png') }}" alt="user" class="profile-pic" /></a>
                     <div class="dropdown-menu dropdown-menu-right scale-up">
                         <ul class="dropdown-user">
                             <li><a href="#"><i class="ti-settings"></i> Account Setting</a></li>
